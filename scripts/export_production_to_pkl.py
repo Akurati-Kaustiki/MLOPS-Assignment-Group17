2bd477870195fb4d24a9a77021075b0e0aef7380
akurati kaustiki <akuratikaustiki@gmail.com>
2025-08-03 13:29:19 +0530

    verifying

--- scripts/export_production_to_pkl.py
+++ scripts/export_production_to_pkl.py
@@ -3,7 +3,7 @@ import joblib
 import os
 
 # Set tracking URI (local mlruns folder)
-# mlflow.set_tracking_uri("file:./mlruns")
+mlflow.set_tracking_uri("file:./mlruns")
 
 # Load the sklearn model directly from the registry
 model = mlflow.sklearn.load_model("models:/IrisClassifier/Production")
